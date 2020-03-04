e677a2bedce16aea3563cabee40d2f32f76b5713
zzinny <wwooww374@gmail.com>
2020-03-04 20:03:10 +0900

    각 내비게이션 메뉴의 클릭 이동주소 변경

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <a href="#default" className="font-effect-anaglyph">Simple Man</a>
+        <a href="/" className="font-effect-anaglyph">Simple Man</a>
           <div className="header-right">
-            <a href="#home">Home</a>
-            <a href="#story">Story</a>
-            <a href="#about">About</a>
+            <a href="/">Home</a>
+            <a href="story">Story</a>
+            <a href="about">About</a>
           </div>
       </header>
       <SlideShow />
